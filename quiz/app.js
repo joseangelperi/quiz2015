55360394a148ea6b3eaf064bbca415e0e4380a33
joseangelperi <joseangelperi@gmail.com>
2015-07-06 20:14:38 +0200

    “Primera página y favicon”
    Eliminamos vistas que no vamos a usar, modificar la pagina principal e
    incluimos favicon

--- quiz/app.js
+++ quiz/app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//2015-07-06 Quitamos el enrutador users   
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -15,15 +16,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+//2015-07-06 Quitamos los comentarios para usar el favicon 
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', routes);
-app.use('/users', users);
+//2015-07-06 Quitamos el enrutador users  
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//2015-07-06 Cambiamos titulo Express por Quiz
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
 
 module.exports = router;
