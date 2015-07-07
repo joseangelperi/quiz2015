2a44df1564a5507129b366a4875653fa474b7eda
joseangelperi <joseangelperi@gmail.com>
2015-07-07 19:07:28 +0200

    Primera pregunta
    Incluimos primera pregunta del juego

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+//2015-07-07 Importamos controlador
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 //2015-07-06 Cambiamos titulo Express por Quiz
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+//2015-07-07 Importamos nuevas vistas
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
